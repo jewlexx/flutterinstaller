48c0e5e0dbc1e20cde51c5bd92f70d71f111c90d
James Cordor <grassojames5@gmail.com>
2022-02-10 11:04:50 +1100

    ADded files

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use dirs::home_dir;
+use git2::Repository;
+use std::fs::create_dir_all;
+
 fn main() {
+    let mut flutter_dir = home_dir().unwrap();
+    flutter_dir.push("Tools");
+    create_dir_all(&flutter_dir);
+    flutter_dir.push("flutter");
+
     println!("Hello, world!");
 }
