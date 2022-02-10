d92d6ece26086ce9d0b5b92ce0eac717bd1da5b6
James Cordor <grassojames5@gmail.com>
2022-02-10 11:30:03 +1100

    Updates

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,18 @@
 use dirs::home_dir;
 use git2::Repository;
+use spinner::SpinnerBuilder;
 use std::fs::create_dir_all;
 
 fn main() {
     let mut flutter_dir = home_dir().unwrap();
     flutter_dir.push("Tools");
-    create_dir_all(&flutter_dir);
+    create_dir_all(&flutter_dir).expect("Failed to create dirs");
     flutter_dir.push("flutter");
+
+    let sp = SpinnerBuilder::new("Cloning flutter repo...".into()).start();
 
     Repository::clone("https://github.com/flutter/flutter.git", flutter_dir)
         .expect("Failed to clone flutter");
+
+    sp.close();
 }
