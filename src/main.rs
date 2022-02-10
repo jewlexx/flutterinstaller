5d8937887e3b0db5e044aadc520a26b15efa6d54
jamesinaxx <grassojames5@gmail.com>
2022-02-10 20:10:34 +1100

    fixes

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,18 @@
 use dirs::home_dir;
 use git2::Repository;
 use spinners_rs::{Spinner, Spinners};
-use std::fs::create_dir_all;
+use std::{
+    fs::create_dir_all,
+    io::{stdout, Write},
+    process::Command,
+    thread::sleep,
+    time::Duration,
+};
+
+#[cfg(target_os = "windows")]
+const FILE_EXTENSION: &str = ".bat";
+#[cfg(not(target_os = "windows"))]
+const FILE_EXTENSION: &str = "";
 
 fn main() {
     let mut flutter_dir = home_dir().unwrap();
@@ -11,8 +22,23 @@ fn main() {
 
     let sp = Spinner::new(&Spinners::Dots, "Cloning flutter repo...".into());
 
-    Repository::clone("https://github.com/flutter/flutter.git", flutter_dir)
-        .expect("Failed to clone flutter");
+    match Repository::clone("https://github.com/flutter/flutter.git", &flutter_dir) {
+        Ok(repo) => {
+            sp.stop();
+            print!("\n");
+            repo
+        }
+        Err(_) => {
+            sp.stop();
+            print!("\n");
+            println!("Failed to clone flutter repo");
+            println!("Check if the directory already exists");
+            return;
+        }
+    };
 
-    sp.stop();
+    flutter_dir.push("bin");
+    flutter_dir.push(format!("flutter{}", FILE_EXTENSION));
+
+    Command::new(&flutter_dir).arg("doctor");
 }
